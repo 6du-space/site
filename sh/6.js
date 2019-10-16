92496fca0a2b98f39f0e926192553992f544cb5a
Tq-Lj <6du@protonmail.com>
2019-10-16 14:13:48 +0800

    .

--- sh/6.js
+++ sh/6.js
@@ -1,6 +1,6 @@
 (async ()=>{
   await navigator.serviceWorker.register('/sw.js', {scope: '/'});
-  var elem=(tag,attr)=>{
+  var tag=(tag,attr)=>{
     var sc = document.createElement(tag);
       for(i in attr){
         sc.setAttribute(i, attr[i]);
@@ -8,16 +8,21 @@
       document.head.appendChild(sc)
 
   }
+  // 请替换为自己的腾讯统计
+  tag('script',{
+    src:'//tajs.qq.com/stats?sId=66475325',
+    async:true
+  })
   let html = await(await fetch('/site.html')).text();
   for(var i of html.split("<")){
     if(i.indexOf('rel="prefetch"')<0){
       let src=i.split('"')[1]
       if(src){
         if(src.endsWith('.js')){
-          elem('script', {src})
+          tag('script', {src})
         }
         else{
-          elem('link', {
+          tag('link', {
             href:src,
             rel:"stylesheet"
           })
