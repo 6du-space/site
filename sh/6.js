05382205465fc9eff118f0ad0e1231780a619523
Tq-Lj <6du@protonmail.com>
2019-10-18 23:17:49 +0800

    .

--- sh/6.js
+++ sh/6.js
@@ -1,5 +1,5 @@
 (async ()=>{
-  await navigator.serviceWorker.register('/sw.js', {scope: '/'});
+  //await navigator.serviceWorker.register('/sw.js', {scope: '/'});
   var tag=(tag,attr)=>{
     var sc = document.createElement(tag);
       for(i in attr){
@@ -9,10 +9,6 @@
 
   }
   // 请替换为自己的腾讯统计
-  tag('script',{
-    src:'//tajs.qq.com/stats?sId=66475325',
-    async:true
-  })
   let html = await(await fetch('/site.html')).text();
   for(var i of html.split("<")){
     if(i.indexOf('rel="prefetch"')<0){
