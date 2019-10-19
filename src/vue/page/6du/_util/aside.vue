3294810a2ffc34aa9321a023b08f31f8943c0139
Tq-Lj <6du@protonmail.com>
2019-10-19 16:57:38 +0800

    .

---
+++ src/vue/page/6du/_util/aside.vue
@@ -0,0 +1,15 @@
+<style lang=stylus scoped></style>
+<template lang=pug>
+ohyeah.aside(width="300px" :noHor="true" thumbColor="rgba(0,0,0,.2)" ref="side")
+  main()
+    header
+      h2.host {{host}}
+      x-ico.x(@click="x=1")
+    .pug(v-html="aside")
+  footer(v-html="foot")
+</template>
+
+<script lang=ls>
+export default _ =
+
+</script>
